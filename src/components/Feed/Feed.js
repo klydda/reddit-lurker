ea560a30d06e7406a825f03bda76c1a579126b2f
klydda <klydda@proton.me>
2024-05-07 16:52:09 +0200

    Fixed faulty comments

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -5,12 +5,11 @@ import { useSelector } from 'react-redux';
 import { useParams, useSearchParams } from 'react-router-dom';
 import { getAllCards } from '../Card/cardSlice';
 
-//TODO: Rewrite to use queryparameters to filter instead of subreddit style (those are needed for subreddits)
 
 function Feed() {
     const allCards = useSelector(getAllCards);
 
-    // Get's the current filter from the URL and filteres the Cards to only show those whose type matches the string of the subreddit. Defaults to all cards if subreddit is empty.
+    // Get's the current filter from the URL and filteres the Cards to only show those whose type matches the string of the filter. Defaults to all cards if filter is empty.
     const [ searchParams ] = useSearchParams();
     const filter = searchParams.get('filter');
     
