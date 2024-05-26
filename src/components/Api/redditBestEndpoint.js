42ec5db4ae67c7f92d135f2d1804b15fe5f0a05c
klydda <klydda@proton.me>
2024-05-26 21:56:44 +0200

    Update getBestFirstEndpoint to dispatch count and after

--- src/components/Api/redditBestEndpoint.js
+++ src/components/Api/redditBestEndpoint.js
@@ -1,5 +1,5 @@
 
-export function getBestPosts(accessToken, dispatch, setCards){
+export function getFirstBestPosts(accessToken, dispatch, setCards, setAfter, setCount){
     const apiUrl = 'https://oauth.reddit.com/best.json';
 
     // Setup the headers you want to include in your request
@@ -22,13 +22,51 @@ export function getBestPosts(accessToken, dispatch, setCards){
     })
     .then(data => {
         const rawPosts = extractRawPosts(data);
+        const after = data.data.after;
+        const count = rawPosts.length;
         dispatch(setCards(rawPosts));
-        // console.log(rawPosts);
+        dispatch(setAfter(after));
+        dispatch(setCount(count));
+        console.log(data);
     })
     .catch(error => {
         console.error('Fetch error:', error);  // Handle any errors that occur during the fetch
     });
 }
+
+export function getNextBestPosts(accessToken, dispatch, setCards, setAfter, setCount){
+    const apiUrl = 'https://oauth.reddit.com/best.json';
+
+    // Setup the headers you want to include in your request
+    const headers = new Headers({
+        'Authorization': `Bearer ${accessToken}`, // Example for an authorization header
+        'Content-Type': 'application/json',               // Setting the content type
+    });
+
+    // Use the fetch API to make the GET request with headers
+    fetch(apiUrl, {
+        method: 'GET',    // Specify the method
+        headers: headers  // Include headers in the request
+    })
+    .then(response => {
+        // Check if the request was successful
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();  // Convert the response body to JSON
+    })
+    .then(data => {
+        const rawPosts = extractRawPosts(data);
+        const after = data.data.after;
+        const count = rawPosts.length;
+        dispatch(setCards(rawPosts));
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);  // Handle any errors that occur during the fetch
+    });
+}
+
 
 function extractRawPosts(data){
     const originalArr = data.data.children;
